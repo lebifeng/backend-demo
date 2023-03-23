800480cba7d40d5062a1c27517f18aab348c62b1
bifeng le <bifengle@bifengdeMac-mini.local>
2023-03-23 20:43:30 +0800

    feat: 优化

--- common/initSql.sql
+++ common/initSql.sql
@@ -1,17 +1,6 @@
-create table user_base_info
-(
-    id      int auto_increment
-        primary key,
-    phone   varchar(11)  null,
-    name    varchar(20)  not null,
-    age     int          null,
-    address varchar(200) null
-);
+create database user;
 
-
-
-
-create table USER.user_base_info
+create table user.user_base_info
 (
     id      int auto_increment
         primary key,
